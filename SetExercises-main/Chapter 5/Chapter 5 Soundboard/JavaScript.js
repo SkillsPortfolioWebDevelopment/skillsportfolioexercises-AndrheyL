8f9a9f8befc0f26e722602b61550e1a6d7e4d9c0
AndrheyL <91404735+AndrheyL@users.noreply.github.com>
2022-04-25 22:34:58 +0400

    Add files via upload

---
+++ SetExercises-main/Chapter 5/Chapter 5 Soundboard/JavaScript.js
@@ -0,0 +1,52 @@
+﻿const sound = ['Aha!', 'Back of the net', 'Out of order', 'Dan', 'Goal', 'Hello', 'Jurassic Park', 'Kiss my face', 'Smell my cheese']
+
+sound.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stop()
+
+
+        document.getElementById(sound).play()
+    })
+
+
+
+    document.getElementById('button').
+        appendChild(btn)
+})
+
+function stop() {
+    sound.forEach(sound => {
+        const song = document.getElementById
+            (sound)
+
+        song.pause()
+        song.currentTime = 0;
+
+    })
+
+}
+////const sounds = ['AHA', 'BackofNet', 'BangOOO', 'Dan', 'Goal', 'HelloPatri', 'JurassicPark', 'KissMyFace', 'SmellMyCheese']
+
+////sounds.forEach(sound => {
+
+////    const btn = document.createElement('music-class')
+
+////    btn.classList.add('btn')
+////    btn.innerText = sound
+////    btn.addEventListener('click', () => {
+
+
+////        document.getElementById(sound).play()
+
+////    })
+
+////    document.getElementById('music-class').
+
+////        appendChild(btn)
+
+////})
