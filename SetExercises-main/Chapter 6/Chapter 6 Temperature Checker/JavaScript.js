8d3bbf63541f75c9d8bf6f0f2539061b04c31ca5
AndrheyL <91404735+AndrheyL@users.noreply.github.com>
2022-04-25 22:36:19 +0400

    Add files via upload

---
+++ SetExercises-main/Chapter 6/Chapter 6 Temperature Checker/JavaScript.js
@@ -0,0 +1,13 @@
+﻿let fahrenheit = document.getElementById("fahrenheit")
+
+let celsius = document.getElementById("celsius")
+
+function fahToCel() {
+    //Used very complicated formula to convert Fahrenheit
+    let output = (parseFloat(fahrenheit.value) 
+        -32) * 0.555556;
+
+    //Fixed the answer so that whenever we type a new number it'll change
+    celsius.value = parseFloat(output.toFixed(2));
+
+}
